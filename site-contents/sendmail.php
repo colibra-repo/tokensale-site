28cd641ae766768adcc550fd5f533200d6a0a7e9
Elena Zlateva <eli@Elenas-MacBook-Pro.local>
2018-11-02 15:05:41 +0200

    add sendemail.php

---
+++ site-contents/sendmail.php
@@ -0,0 +1,47 @@
+<?php
+
+/* 
+$_POST variables
+	- email
+	- firstName
+	- lastName
+	- country
+	- amount
+	- question
+
+	usage: $_POST['variable'];
+*/
+
+$to      = $_POST['email']; 
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+/* 
+
+Changes required in sendmail.ini
+
+Mail Server Configuration - change with your server details
+
+smtp_server=smtp.gmail.com
+smtp_port=587
+error_logfile=error.log
+debug_logfile=debug.log
+auth_username=your-gmail-id@gmail.com
+auth_password=your-gmail-password
+force_sender=your-gmail-id@gmail.com
+
+Changes required in php.ini
+
+SMTP=smtp.gmail.com
+smtp_port=587
+sendmail_from = your-gmail-id@gmail.com
+sendmail_path = "\"C:\xampp\sendmail\sendmail.exe\" -t"
+;sendmail_path = "C:\xampp\mailtodisk\mailtodisk.exe"
+
+*/
+?>
